ed9cd4c210ae45a79eeac9a106c7b5555365db7b
mariacivica <maria.gonzalez@civica-soft.com>
2024-06-25 09:19:15 +0200

    Add initial project files and configurations

---
+++ monster-angular-lite-master/package/src/app/dashboard/dashboard-components/blog-cards/blog-cards-data.ts
@@ -0,0 +1,35 @@
+export interface blogcard {
+    title: string,
+    subtitle: string,
+    subtext: string,
+    image: string
+}
+
+export const blogcards: blogcard[] = [
+
+    {
+        title: 'Ingeniería de Datos',
+        subtitle: '',
+        subtext: 'Diseño, construcción y gestión de infraestructuras y sistemas que permiten recopilar, almacenar, y procesar  grandes volúmenes de datos.',
+        image: 'assets/images/bg/bg1.jpg'
+    },
+    {
+        title: 'Análisis de Datos',
+        subtitle: '',
+        subtext: 'Proceso de inspeccionar, limpiar y modelar datos con el objetivo de descubrir información útil, llegar a conclusiones y apoyar la toma de decisiones.',
+        image: 'assets/images/bg/bg2.jpg'
+    },
+    {
+        title: 'Frontend',
+        subtitle: '',
+        subtext: 'Parte del desarrollo web que se centra en el diseño y la implementación de la interfaz de usuario (interacción con la app o sitio web).',
+        image: 'assets/images/bg/bg3.jpg'
+    },
+    {
+        title: 'Backend',
+        subtitle: '',
+        subtext: 'Parte del desarrollo web que se encarga del servidor, base de datos y la lógica de la aplicación. Gestiona, procesa datos y garantiza el buen funcionamiento.',
+        image: 'assets/images/bg/bg4.jpg'
+    },
+
+]
