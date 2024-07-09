f0a000cddb64e5cfcc319500863eed663df1cfd4
mariacivica <maria.gonzalez@civica-soft.com>
2024-07-09 14:34:37 +0200

    delete table component

--- monster-angular-lite-master/package/src/app/shared/sidebar/menu-items.ts
+++ monster-angular-lite-master/package/src/app/shared/sidebar/menu-items.ts
@@ -4,7 +4,7 @@ export const ROUTES: RouteInfo[] = [
 
   {
     path: '/dashboard',
-    title: 'Panel de control',
+    title: 'Inicio',
     icon: 'bi bi-speedometer2',
     class: '',
     extralink: false,
@@ -22,54 +22,6 @@ export const ROUTES: RouteInfo[] = [
     path: '/component/badges',
     title: 'Badges',
     icon: 'bi bi-patch-check',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/buttons',
-    title: 'Botón',
-    icon: 'bi bi-hdd-stack',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/card',
-    title: 'Tarjeta',
-    icon: 'bi bi-card-text',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/dropdown',
-    title: 'Desplegable',
-    icon: 'bi bi-menu-app',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/pagination',
-    title: 'Paginación',
-    icon: 'bi bi-dice-1',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/nav',
-    title: 'Navegación',
-    icon: 'bi bi-pause-btn',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/table',
-    title: 'Tabla',
-    icon: 'bi bi-layout-split',
     class: '',
     extralink: false,
     submenu: []
