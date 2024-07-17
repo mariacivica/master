908e0a7fb6a6fb869161e51ed923cb6fac1c179e
mariacivica <maria.gonzalez@civica-soft.com>
2024-07-17 21:00:17 +0200

    delete components dashboard and update ingenieria-datos

--- monster-angular-lite-master/package/src/app/dashboard/dashboard.module.ts
+++ monster-angular-lite-master/package/src/app/dashboard/dashboard.module.ts
@@ -4,8 +4,6 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { Routes, RouterModule } from "@angular/router";
 import { NgApexchartsModule } from "ng-apexcharts";
 import { DashboardComponent } from "./dashboard.component";
-import { SalesRatioComponent } from "./dashboard-components/sales-ratio/sales-ratio.component";
-import { FeedsComponent } from "./dashboard-components/feeds/feeds.component";
 import { TopSellingComponent } from "./dashboard-components/top-selling/top-selling.component";
 import { TopCardsComponent } from "./dashboard-components/top-cards/top-cards.component";
 import { BlogCardsComponent } from "./dashboard-components/blog-cards/blog-cards.component";
@@ -40,8 +38,6 @@ const routes: Routes = [
   ],
   declarations: [
     DashboardComponent,
-    SalesRatioComponent,
-    FeedsComponent,
     TopSellingComponent,
     TopCardsComponent,
     BlogCardsComponent,
