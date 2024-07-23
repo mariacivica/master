3b00e88ec6819fc9dee2d5e10b8b517553ebcc49
mariacivica <maria.gonzalez@civica-soft.com>
2024-07-23 12:22:05 +0200

    end roles-cards

--- monster-angular-lite-master/package/src/app/recomendations/roles-cards/roles-cards-data.ts
+++ monster-angular-lite-master/package/src/app/recomendations/roles-cards/roles-cards-data.ts
@@ -2,37 +2,37 @@ export interface RoleCard {
   title: string,
   subtitle: string,
   subtext: string,
-  icon: string, // Añadir esta propiedad
+  icon: string,
   route: string
 }
 
 export const roleCards: RoleCard[] = [
   {
-      title: 'Data Architect',
-      subtitle: '',
-      subtext: 'Diseño y gestión de la arquitectura de datos, asegurando la integración y calidad de los datos.',
-      icon: 'bi-diagram-3', // Icono para Data Architect
-      route: 'data-architect'
+    title: 'Data Architect',
+    subtitle: '',
+    subtext: 'Diseño y gestión de la arquitectura de datos, asegurando la integración y calidad de los datos.',
+    icon: 'bi-diagram-3',
+    route: 'data-architect'
   },
   {
-      title: 'Frontend Developer',
-      subtitle: '',
-      subtext: 'Desarrollo de la interfaz de usuario de aplicaciones web, garantizando una experiencia de usuario fluida y atractiva.',
-      icon: 'bi-display', // Icono para Frontend Developer
-      route: 'frontend-developer'
+    title: 'Frontend Developer',
+    subtitle: '',
+    subtext: 'Desarrollo de la interfaz de usuario de aplicaciones web, garantizando una experiencia de usuario fluida y atractiva.',
+    icon: 'bi-display',
+    route: 'frontend-developer'
   },
   {
-      title: 'Data Engineer',
-      subtitle: '',
-      subtext: 'Construcción y mantenimiento de infraestructuras de datos escalables y eficientes.',
-      icon: 'bi-cpu', // Icono para Data Engineer
-      route: 'data-engineer'
+    title: 'Data Engineer',
+    subtitle: '',
+    subtext: 'Construcción y mantenimiento de infraestructuras de datos escalables y eficientes.',
+    icon: 'bi-cpu',
+    route: 'data-engineer'
   },
   {
-      title: 'Project Management',
-      subtitle: '',
-      subtext: 'Planificación, ejecución y supervisión de proyectos, asegurando el cumplimiento de plazos y objetivos.',
-      icon: 'bi-clipboard-data', // Icono para Project Management
-      route: 'project-management'
+    title: 'Project Management',
+    subtitle: '',
+    subtext: 'Planificación, ejecución y supervisión de proyectos, asegurando el cumplimiento de plazos y objetivos.',
+    icon: 'bi-clipboard-data',
+    route: 'project-management'
   }
 ];
